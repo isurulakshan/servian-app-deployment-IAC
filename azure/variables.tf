0a2d287d027a75bcef189e3dbc56993aec76ae37
Isuru Chiththananda <isuru@Isurus-MacBook-Pro.local>
2021-11-27 11:25:43 +0530

    Initial commit with Azure Linux VM Provisioning steps

---
+++ azure/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  default = "RG-Isuru-TF"
+}
+variable "location" {
+  default = "westus2"
+}
+
+variable "environment" {
+  default = "PRD"
+}
+
+variable "deployment_script" {
+  default = "deploy.sh"
+}
