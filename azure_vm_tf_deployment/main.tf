7fc7a4a4fcbe283e3e60e942da912f4295ea5157
Isuru Chiththananda <isuru@Isurus-MacBook-Pro.local>
2021-11-30 12:05:28 +0530

    Changes: Azure provider with spn configurations

--- azure_vm_tf_deployment/main.tf
+++ azure_vm_tf_deployment/main.tf
@@ -7,9 +7,14 @@ terraform {
   }
   required_version = ">= 0.14.9"
 }
-
+#Azure provider with spn configurations
 provider "azurerm" {
   features {}
+
+  subscription_id             = var.subscription_id
+  client_id                   = var.client_id
+  client_secret               = var.client_secret
+  tenant_id                   = var.tenant_id
 }
 
 #Create azure resource group
--- azure_vm_tf_deployment/variables.tf
+++ azure_vm_tf_deployment/variables.tf
@@ -1,10 +1,31 @@
+#Target customer subscription
+variable "subscription_id" {
+  type = string
+}
+#Azure SPN client id
+variable "client_id" {
+  type = string
+}
+#Azure SPN client secret
+variable "client_secret" {
+  type = string
+}
+#Azure tenant id
+variable "tenant_id" {
+  type = string
+}
+#Azure location
 variable "location" {
   default = "westus2"
+  type    = string
 }
+#Customer environment for resource tags
 variable "environment" {
   default = "PRD"
+  type    = string
 }
-
+#Customer name
 variable "customer_name" {
   default = "servian"
+  type    = string
 }
